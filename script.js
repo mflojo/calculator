c4fdd649288719f7dc74df56df57934a0eccfcf2
mflojo <m.eric.flojo@gmail.com>
2025-02-02 20:17:59 -0800

    Add operate function to script

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ const subtract = (num1, num2) => num1 - num2;
 const multiply  = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
-console.log(add(10, 5));
-console.log(subtract(10, 5));
-console.log(multiply(10, 5));
-console.log(divide(10, 5));
-
 let num1 = 0;
 let num2 = 0;
 let operator = "";
+
+let operate = function(num1, num2, operator) {
+    if (operator === "+") {
+        return add(num1, num2);
+    } else if (operator === "-") {
+        return subtract(num1, num2);
+    } else if (operator === "*") {
+        return multiply(num1, num2);
+    } else if (operator === "/") {
+        return divide(num1, num2);
+    } else {
+        alert("ERROR. Incorrect input found!");
+    }
+};
+
+console.log(operate(10, 5, "*"));
